ba97c75649c70e24724de0a1dee39f3f28c1a170
WesleyHaring <D284479@edu.rocwb.nl>
2021-09-27 12:11:52 +0200

    Ticket 2b

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,23 @@
 
 <footer>
-	© {{ __('misc.copyright') }}
+    <div class="footerOverOns">
+        <h3 class="footerOverOnsH3">Over Ons</h3>
+        <p class="footerOverOnsP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet consectetur et eum ex, id maxime, nam non odit officiis praesentium quae quo quod repellendus vel veritatis voluptatem voluptates voluptatum!</p>
+    </div>
+    <div class="footerContact">
+        <h3 class="footerContactH3">Contact</h3>
+        <p class="footerContactP">Handleidingen</p>
+        <p class="footerContactP">4891NW, Rijsbergen</p>
+        <p class="footerContactP">06-12345678</p>
+    </div>
+    <div class="footerSocials">
+        <h3 class="footerSocialsH3">Socials</h3>
+        <p class="footerSocialsP">instagram: @instagram</p>
+        <p class="footerSocialsP">Facebook: @facebook</p>
+        <p class="footerSocialsP">Twitter: @twitter</p>
+    </div>
+    © {{ __('misc.copyright') }}
+
 </footer>
 
 
